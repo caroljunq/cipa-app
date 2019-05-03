b5f223bee1ed23d4d553e9e98b0754b441d9c58a
Leonardo Oliveira <leosoliveira10@gmail.com>
2019-05-02 21:32:35 -0300

    página do perfil feita

--- src/app/pages/change-password/change-password.page.ts
+++ src/app/pages/change-password/change-password.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-change-password',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePasswordPage implements OnInit {
 
-  constructor() { }
+  oldPassword: string; 
+  newPassword: string; 
+  cfmPassword: string; 
+
+  constructor(public toastController: ToastController) { }
 
   ngOnInit() {
   }
 
+  changePassword() {
+      console.log(this.oldPassword, this.newPassword, this.cfmPassword); 
+
+      if(this.oldPassword == undefined || this.newPassword == undefined || this.cfmPassword == undefined) {
+          this.PresentToast('Preencha todos os campos!', 2000); 
+      } else if(this.newPassword != this.cfmPassword) {
+          this.PresentToast('As senhas não são iguais', 3000); 
+      } else {
+          // Confirmar se a senha antiga é a correta 
+      }
+  }
+
+  async PresentToast(message, duration) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: duration,
+      color: 'dark'
+    });
+    toast.present();
+  }
 }
